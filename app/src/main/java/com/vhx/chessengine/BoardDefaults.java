b5b60e5a35bb3992b8dd48ced09df540edd03a00
vhxLUA-max <nageraadrian51@gmail.com>
2026-09-20 20:22:17 +0800

    Add Android board defaults

---
+++ app/src/main/java/com/vhx/chessengine/BoardDefaults.java
@@ -0,0 +1,8 @@
+package com.vhx.chessengine;
+
+public final class BoardDefaults {
+    public static final String START_FEN =
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+    private BoardDefaults() {}
+}
