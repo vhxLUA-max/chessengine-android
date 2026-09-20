27c2f8ac90367b7857933dd65b842f25273ea310
vhxLUA-max <nageraadrian51@gmail.com>
2026-09-21 06:49:23 +0800

    Use preinstalled Android NDK and CMake versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         targetSdk = 37
         versionCode = 1
         versionName = "1.0.0"
-        ndkVersion = "27.2.12479018"
+        ndkVersion = "27.3.13750724"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a")
@@ -28,7 +28,7 @@ android {
     externalNativeBuild {
         cmake {
             path = file("src/main/cpp/CMakeLists.txt")
-            version = "3.22.1"
+            version = "3.31.5"
         }
     }
 
